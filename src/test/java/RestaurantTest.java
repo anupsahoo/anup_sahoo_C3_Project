21874cf5f7e9e5c94e07f9fe2ad5699a1567f24d
anupsahoo <testinganupsahoo@gmail.com>
2022-01-05 20:09:47 +0530

    Part 3: Failing test case

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -76,7 +76,15 @@ class RestaurantTest {
     //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 	
 	
-
+	//>>>>>>>>>>>>>>>>>>>>>>>>>>>GetCost<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+		@Test
+    public void select_item_from_list_should_return_order_cost(){
+            int totalCost;
+            createMockRestaurant();
+            List<String> selectedItemNames = Arrays.asList("Sweet corn soup", "Vegetable lasagne" ) ;
+            totalCost = restaurant.getTotalCost(selectedItemNames) ;
+            assertEquals(388, totalCost);
+    }
 	
 
 	
