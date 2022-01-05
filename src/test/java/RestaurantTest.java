07246a70f44b55140419e3ad856908cf45b46633
anupsahoo <testinganupsahoo@gmail.com>
2022-01-05 20:13:32 +0530

    Part 3: Solution

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -80,6 +80,7 @@ class RestaurantTest {
 		@Test
     public void select_item_from_list_should_return_order_cost(){
             int totalCost;
+            
             createMockRestaurant();
             List<String> selectedItemNames = Arrays.asList("Sweet corn soup", "Vegetable lasagne" ) ;
             totalCost = restaurant.getTotalCost(selectedItemNames) ;
